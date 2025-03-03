f4cac373691fe71d432b34c7da2ab0dc43148043
GellyBird <147560865+GellyBird@users.noreply.github.com>
2025-03-03 16:43:53 +0000

    push

--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -30,11 +30,28 @@ public class Player{
         // 0=♠,1=♥,2=♣,3=♦
 
         for (Integer suits : suitFreq) {
+            String cardsAvailable = "";
             if (suits >= 5) {
-
-            }
+                for (int i = rankFreq.size() - 1; i > rankFreq.size() - 5; i --) {
+                    cardsAvailable +=  rankFreq.get(i);
+                } if (cardsAvailable.equals("11111")) {
+                    return "Royal Flush";
+                } else {
+                    cardsAvailable = "";
+                    for (int i = rankFreq.size() - 1; i > 0; i --) {
+                        cardsAvailable += rankFreq.get(i);
+                    }
+                    if (cardsAvailable.contains("11111")) {
+                        return "Straight Flush";
+                    } else {
+                        return "Flush";
+                    }
+                }
+            } 
         }
-
+        for (Integer ranks : rankFreq) {
+            
+        }
         return "Nothing";
     }
 
